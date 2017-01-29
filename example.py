906f66454789be81dcee162f81cef1d17bb9539c
Russell Keith-Magee <russell@keith-magee.com>
2017-01-29 12:09:39 +0800

    Updated example to use data from inside the demo itself.

--- example.py
+++ example.py
@@ -1,19 +1,20 @@
-
 import toga
 
 
-def button_handler(widget):
-    print("Hello")
-
-
 class Example(toga.App):
+    def button_handler(self, widget):
+        print("Hello,", self.text.value())
+
     def startup(self):
         self.main_window = toga.MainWindow(self.name)
         self.main_window.app = self
 
         box = toga.Box()
 
-        button = toga.Button('Hello world', on_press=button_handler)
+        self.text = toga.TextInput(placeholder='something...')
+        box.add(self.text)
+
+        button = toga.Button('Hello world', on_press=self.button_handler)
 
         button.style.set(margin=50)
 
